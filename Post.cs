a3270e3ab1536d18e4a7bfe3e5f3421b4ca0fc57
Alexandru Iacoboae <alexandru.iacoboae@certent.com>
2020-04-30 17:34:15 +0300

    Add project files.

---
+++ Post.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PostVoteInConsoleApp
+{
+    partial class Program
+    {
+        public class Post
+        {
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public DateTime CreationTime { get; set; }
+
+            public int UpVote { get; private set; }
+
+            public int DownVote { get; private set; }
+
+
+            public void UpVoteMethod()
+            {
+                UpVote++;
+            }
+            
+            public void DownVoteMethod()
+            {
+                DownVote++;
+            }
+
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+namespace PostVoteInConsoleApp
+{
+    partial class Program
+    {
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            var post = new Post();
+
+            Console.WriteLine("Enter Post Title:");
+            var postTitle = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(postTitle))
+                post.Title = postTitle;
+
+            Console.WriteLine("Enter Post Description:");
+            var postDescription = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(postDescription))
+                post.Description = postDescription;
+
+            post.CreationTime = DateTime.Now;
+
+
+
+            Console.WriteLine("Please cast your vote about this post:\n" +
+                "1: Upvote\n" +
+                "2: Downvote\n" +
+                "3: Exit");
+            
+            var command = Console.ReadLine();
+
+            while (!string.IsNullOrEmpty(command) && (command.ToLower() != "3"))
+
+                switch(command.ToLower())
+                {
+                    case "1":
+                        post.UpVoteMethod();
+                        Console.WriteLine("Cast your vote vote option again or press 3 to exit");
+                        command = Console.ReadLine();
+                        break;
+
+                    case "2":
+                        post.DownVoteMethod();
+                        Console.WriteLine("Cast your vote vote option again or press 3 to exit");
+                        command = Console.ReadLine();
+                        
+                        break;
+
+                    case "3":
+                        break;
+
+                    default:
+                        Console.WriteLine("Please write a valid command: Upvote | Downvote | Exit");
+                        command = Console.ReadLine();
+                        break;
+
+                }
+
+            Console.WriteLine("Post title:    {0} \n" +
+                              "Description:   {1} \n" +
+                              "It has {2} likes \n" +
+                              "It has {3} dislikes \n" +
+                              " and was created at {4}", 
+                                post.Title, post.Description, post.UpVote, post.DownVote, post.CreationTime); ;
+
+
+        }
+    }
+}
